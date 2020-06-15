04f0a4bc8e256c025c7cb661ba64d347f8d9e042
Dhruv Patel <dhruvpatel.aj@gmail.com>
2020-06-14 19:39:48 -0600

    spotify authorization

--- src/App.js
+++ src/App.js
@@ -2,13 +2,43 @@ import React from "react";
 import "./App.css";
 import { Login } from "./components";
 
-function App() {
-    return (
-        <div className="App">
-            <h1> App.js </h1>
-            <Login />
-        </div>
-    );
+import axios from "axios";
+import {
+    spotifyAPI_url,
+    client_id,
+    client_secret,
+    redirect_URI,
+} from "./configs";
+
+class App extends React.Component {
+    state = {
+        value: "SpotifyDJ",
+        authToken: "",
+        authorized: false,
+    };
+
+    componentDidMount = () => {
+        let url = window.location.href;
+        if (url.indexOf("token=") > -1) {
+            let authToken = url.split("token=")[1].split("&")[0].trim();
+            let authorized = true;
+            this.setState({ authToken, authorized });
+        }
+    };
+
+    authorize = () => {
+        console.log("AUTHORIZE");
+        window.location.assign(spotifyAPI_url);
+    };
+    render() {
+        return (
+            <div className="App">
+                <h1> App.js </h1>
+                <button onClick={this.authorize}>Log in</button>
+                <h1> Test</h1>
+            </div>
+        );
+    }
 }
 
 export default App;
