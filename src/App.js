e50c6ae0a883e48d123b22e238cf827f1edadef3
Dhruv Patel <dhruvpatel.aj@gmail.com>
2020-06-14 19:05:16 -0600

    simple login test 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import { Login } from "./components";
 
 function App() {
     return (
         <div className="App">
             <h1> App.js </h1>
+            <Login />
         </div>
     );
 }
